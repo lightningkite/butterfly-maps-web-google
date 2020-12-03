4325d26821e637482bb4ec5c44e4ac51db6c7ba5
Joseph Ivie <joseph@lightningkite.com>
2020-12-03 12:29:10 -0700

    Package names adjusted to match repo name

--- dist/LatLng.ext.d.ts
+++
@@ -1,3 +0,0 @@
-import { GeoCoordinate } from "butterfly/dist/location/GeoCoordinate";
-export declare function xLatLngToButterfly(this_: google.maps.LatLng): GeoCoordinate;
-export declare function xGeoCoordinateToMaps(this_: GeoCoordinate): google.maps.LatLng;
